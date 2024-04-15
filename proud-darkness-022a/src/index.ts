acea83c0d96d7e263ba3e3fcf36c7846f47fd7b4
moskalyk <morgan.moskalyk@protonmail.ch>
2024-04-15 13:05:16 -0400

    added template

--- proud-darkness-022a/src/index.ts
+++ proud-darkness-022a/src/index.ts
@@ -80,24 +80,9 @@ const Strings = (base: any) => {
 	base.toSnakeCase = (str: any) => {
 		return str.toLowerCase().replace(/\s+/g, '_');
 	}
+
 	base.removeCharacter = (str: any, charToRemove: any)=>{
 		return str.replace(new RegExp(charToRemove, 'g'), '');
-	}
-
-	base.isValidKey = (data: any) => {
-		try {
-			// Check if 'data' is an object and has 'inference' property
-			if (typeof data === 'object' && data !== null && 'inference' in data) {
-				// Check if 'inference' has 'images' property which is an array
-				if (Array.isArray(data.inference.images) && data.inference.images.length > 0) {
-					// Check if the first element of 'images' array has 'url' property
-					return 'url' in data.inference.images[0]
-				}
-			}
-		} catch (error) {
-			console.error("An error occurred:", error)
-		}
-		return false;
 	}
 
 	base.formatStatString = (str: any, main = true) => {
@@ -151,17 +136,12 @@ const Time = (base: any) => {
 
 const Upload = (base: any) => {
 	base.uploadAsset = async (projectID: any, collectionID: any, assetID: any, tokenID: any, url: any) => {
-		const response = await fetch(url);
-		if (!response.ok) throw new Error(`Failed to fetch file from ${url}: ${response.statusText}`);
-		const arrayBuffer = await response.arrayBuffer();
-		const blob = new Blob([arrayBuffer]);
-
-		const formData = new FormData();
 		
-		formData.append('file', blob, `image.png`); // You might want to dynamically determine the filename
+		// TODO: fetch media url
+
+		// TODO: create form data from file blob
 	  
 		let METADATA_URL;
-
 		if(base.env.DEV){
 			METADATA_URL = 'https://dev-metadata.sequence.app'
 		} else {
@@ -172,22 +152,12 @@ const Upload = (base: any) => {
 		const endpointURL = `${METADATA_URL}/projects/${projectID}/collections/${collectionID}/tokens/${tokenID}/upload/${assetID}`;
 
 		try {
-		  // Use fetch to make the request
-		  const fetchResponse = await fetch(endpointURL, {
-			method: 'PUT',
-			body: formData,
-			headers: {
-			  'X-Access-Key': base.env.PROJECT_ACCESS_KEY,
-			  'Authorization': `Bearer ${base.env.JWT_ACCESS_KEY}`, // Put your token here
-			},
-		  });
-	  
-		  // Assuming the response is JSON
-		  const data = await fetchResponse.json();
+			// TODO: upload media asset
 
-		  return data;
-		}catch(err){
+			return {};
+		}catch(err: any){
 			console.log(err)
+			throw new Error(err)
 		}
 	}
 
@@ -210,71 +180,18 @@ const Upload = (base: any) => {
 				const collectionID = base.env.COLLECTION_ID
 				const projectID = base.env.PROJECT_ID
 
-				// tokenID
-				const randomNonceSpace = ethers.BigNumber.from(ethers.utils.hexlify(ethers.utils.randomBytes(20)))
+				// assign a randomNonceSpace as tokenID
+				const randomTokenIDSpace = ethers.BigNumber.from(ethers.utils.hexlify(ethers.utils.randomBytes(20)))
 
-				// create token
-				const raw2 = JSON.stringify({
-					"projectID": projectID,
-					"collectionID": collectionID,
-					"token": {
-						"tokenId": String(randomNonceSpace),
-						"name": name,
-						"description": "A free lootbox mini-game available for use in any game that requires collectible rewards",
-						"decimals": 0,
-						"attributes": attributes
-					}
-				});
+				// TODO: create token
 
-				const requestOptions = {
-					method: "POST",
-					headers: myHeaders,
-					body: raw2,
-				};
+				// TODO: create asset
 
-				const res2 = await fetch(`${METADATA_URL}/rpc/Collections/CreateToken`, requestOptions)
-				const json2: any = await res2.json()
+				// TODO: upload asset
 
-				// create asset
-				const raw3 = JSON.stringify({
-					"projectID": projectID,
-					"asset": {
-						"collectionId": collectionID,
-						"tokenId": String(randomNonceSpace),
-						"metadataField": "image"
-					}
-				});
+				// TODO: update token to not be private
 
-				const requestOptions3 = {
-					method: "POST",
-					headers: myHeaders,
-					body: raw3,
-				};
-
-				const res3 = await fetch(`${METADATA_URL}/rpc/Collections/CreateAsset`, requestOptions3)
-				const json3: any = await res3.json()
-
-				// upload asset
-				const uploadAssetRes = await base.uploadAsset(projectID, collectionID, json3.asset.id, String(randomNonceSpace), imageUrl)
-
-				const raw4 = JSON.stringify({
-					"projectID": projectID,
-					"collectionID": collectionID,
-					"private": false,
-					"tokenID": String(randomNonceSpace)
-				});
-
-				const requestOptions4 = {
-					method: "POST",
-					headers: myHeaders,
-					body: raw4,
-				};
-
-				// update token to not be private
-				const res4 = await fetch(`${METADATA_URL}/rpc/Collections/UpdateToken`, requestOptions4)
-				const json4 = await res4.json()
-
-				return {url: uploadAssetRes.url, tokenID: String(randomNonceSpace)}
+				return {url: 'https://', tokenID: String(randomTokenIDSpace)}
 			}catch(err){
 				console.log(err)
 			}
@@ -283,47 +200,13 @@ const Upload = (base: any) => {
 	return base;
 };
 
-function capitalizeFirstWord(str: any) {
-	// Check if the string is not empty
-	if (str.length === 0) return str;
-  
-	// Convert the first character to uppercase and concatenate the rest of the string
-	return str.charAt(0).toUpperCase() + str.slice(1);
-  }
-
 const Inference = (base: any) => {
 	return {
 		...base,
 		getInferenceWithItem: async (prompt: any) => {
 			return new Promise( async (res) => {
-				fetch(`https://api.cloud.scenario.com/v1/models/${base.env.SCENARIO_MODEL_ID}/inferences`, {
-					method: 'POST',
-					headers: {
-						'Authorization': `Basic ${base.env.SCENARIO_API_KEY}`,
-						'accept': 'application/json',
-						'content-type': 'application/json'
-					},
-					body: JSON.stringify({
-								"parameters": {
-								"numSamples": 1,
-								"qualityBoostScale": 4,
-								"qualityBoost": false,
-								"type": "txt2img",
-								"disableMerging": false,
-								"hideResults": false,
-								"referenceAdain": false,
-								"intermediateImages": false,
-								"scheduler": 'EulerDiscreteScheduler',
-								"referenceAttn": false,
-								"prompt": prompt + ' single object on black background no people'
-							}
-						})
-				})
-				.then(response => response.json())
-				.then((data: any) => {
-					res({inferenceId: data.inference.id})
-				})
-				.catch(error => console.error('Error:', error));
+				// TODO: implement performing an inference on the prompt
+				res({})
 			})
 		  },
 		 getInferenceStatus: (id: any) => {
@@ -340,38 +223,8 @@ const Inference = (base: any) => {
 
 				// Function to poll the inference status
 				const pollInferenceStatus = async (res: any) => {
-					let status = '';
-					let inferenceData: any = null;
-					while (!['succeeded', 'failed'].includes(status)) {
-						// Fetch the inference details
-						try {
-							const inferenceResponse = await fetch(`https://api.cloud.scenario.com/v1/models/Fm1gtd_gRwmopwj2gyWtUA/inferences/${inferenceId}`, {
-								method: 'GET',
-								headers
-							});
-							if (inferenceResponse.ok) {
-								console.log(inferenceResponse.statusText)
-								inferenceData = await inferenceResponse.json();
-								
-							}
-						}catch(err){
-							console.log(err)
-						}
-						status = inferenceData.inference.status;
-						console.log(`Inference status: ${status}`);
-
-						// Wait for a certain interval before polling again
-						await new Promise(resolve => setTimeout(resolve, 5000)); // Polling every 5 seconds
-					}
-					// Handle the final status
-					if (status === 'succeeded') {
-						console.log('Inference succeeded!');
-						console.log(inferenceData); // Print inference data
-						res(inferenceData)
-					} else {
-						console.log('Inference failed!');
-						console.log(inferenceData); // Print inference data
-					}
+					// TODO: implement polling on status of response
+					res({})
 				};
 
 				// Start polling the inference status
@@ -381,77 +234,9 @@ const Inference = (base: any) => {
 	}
 }
 
-// Works in both a Webapp (browser) or Node.js:
-import { SequenceIndexer } from '@0xsequence/indexer'
-
-const isLessThan24Hours = (isoDate: string) => {
-    const dateProvided: any = new Date(isoDate);
-    const currentDate: any = new Date();
-    const twentyFourHours = 24 * 60 * 60 * 1000; // 24 hours in milliseconds
-
-    // Calculate the difference in milliseconds
-    const difference = currentDate - dateProvided;
-
-    // Check if the difference is less than 24 hours
-    return difference < twentyFourHours && difference > 0;
-}
-
-const fullPaginationDay = async (env: Env, address: string) => {
-    const txs: any = []
-	const indexer = new SequenceIndexer(`https://${env.CHAIN_HANDLE}-indexer.sequence.app`, env.PROJECT_ACCESS_KEY)
-
-    // here we query the Joy contract address, but you can use any
-    const filter = {
-        accountAddress: address,
-    };
-
-    // query Sequence Indexer for all token transaction history on Mumbai
-
-	let txHistory: any
-	let firstLoop = true;
-    // if there are more transactions to log, proceed to paginate
-    while(firstLoop || txHistory.page.more){  
-		if(firstLoop){
-			txHistory = await indexer.getTransactionHistory({
-				filter: filter,
-				page: { pageSize: 50 }
-			})
-		}
-		firstLoop = false
-        txHistory = await indexer.getTransactionHistory({
-            filter: filter,
-            page: { 
-                pageSize: 50, 
-                // use the after cursor from the previous indexer call
-                after: txHistory!.page!.after! 
-            }
-        })
-		for(let i = 0; i < txHistory.transactions.length; i++){
-			if(!isLessThan24Hours(txHistory.transactions[i].timestamp)){
-				break;
-			}
-			txs.push(txHistory.transactions[i])
-		}
-    }
-
-    return txs
-}
-
-const hasDailyMintRetriction = async (env: Env, address: string) => {
-	let count = 0
-	const txs = await fullPaginationDay(env, address)
-	for(let i = 0; i < txs.length; i++){
-		if(txs[i].transfers[0].from == '0x0000000000000000000000000000000000000000') count++
-	}
-	return count < env.DAILY_MINT_RESTRICTION
-}
-
 async function handleRequest(request: any, env: Env, ctx: ExecutionContext) {
 
-	const originUrl = new URL(request.url);
-	const referer = request.headers.get('Referer');
-
-	if(referer.toString() == env.CLIENT_URL || referer.toString() == 'http://localhost:5173/'){
+		// handle pre-request call
 		if (request.method === "OPTIONS") {
 			return new Response(null, {
 				headers: {
@@ -475,18 +260,14 @@ async function handleRequest(request: any, env: Env, ctx: ExecutionContext) {
 
 		try {
 
+			// TODO: (optional) implement address restriction
+
+			// assign the correct project access key based on the environment
 			if(env.DEV) env.PROJECT_ACCESS_KEY = env.PROJECT_ACCESS_KEY_DEV
 			else env.PROJECT_ACCESS_KEY = env.PROJECT_ACCESS_KEY_PROD
 
 			const payload = await request.json()
 			const { address, tokenID, mint }: any = payload
-			const chainConfig: any = findSupportedNetwork(env.CHAIN_HANDLE)
-
-			if(address.toLowerCase() != env.ADMIN.toLowerCase()){
-				if(!await hasDailyMintRetriction(env, address)){
-					return new Response(JSON.stringify({limitExceeded: true}), { status: 400 })
-				}
-			}
 
 			let lootbox = ProcessInferencePool(
 				Inference(
@@ -504,27 +285,23 @@ async function handleRequest(request: any, env: Env, ctx: ExecutionContext) {
 
 			if(mint){
 				try {
-					const txn = await callContract(env, chainConfig, env.CONTRACT_ADDRESS, address, tokenID)
-					return new Response(JSON.stringify({txnHash: txn.hash}), { status: 200 })
+					// TODO: implement a callContract requst to mint token
+					const txn = await callContract(env, env.CONTRACT_ADDRESS, address, tokenID)
+					return new Response(JSON.stringify({txnHash: txn}), { status: 200 })
 				} catch(error: any) {
-					console.log(error)
 					return new Response(JSON.stringify(error), { status: 400 })
 				}
 			} else {
 				const loot = await lootbox.generate()
-				const id = await lootbox.getInferenceWithItem(loot.loot.name)
-				const resObject = await lootbox.getInferenceStatus(id)
-				const response = await lootbox.upload(loot.loot.name + " " + loot.loot.type, loot.attributes, resObject.inference.images[0].url)
-				return new Response(JSON.stringify({loot: loot, image: response.url, name: loot.loot.name, tokenID: response.tokenID}), { status: 200 });
+				// TODO: get inference with item
+				// TODO: poll to get inference status with id
+				// TODO: upload loot and media
+				return new Response(JSON.stringify({loot: loot, image: 'https://', name: 'lootbox item', tokenID: '0'}), { status: 200 });
 			}
 		} catch (error) {
 			console.log(error)
 			return new Response('Error', { status: 500 }); // Handle errors
 		}
-	} else {
-		return new Response('Bad Origin', { status: 500 }); // Handle errors
-	}
-
 }
 
 export default {
@@ -542,7 +319,9 @@ export default {
 	}
 };
 
-const callContract = async (env: Env, chainConfig: NetworkConfig, collectionAddress: string, address: string, tokenID: number): Promise<ethers.providers.TransactionResponse> => {
+const callContract = async (env: Env, collectibleAddress: string, address: string, tokenID: number): Promise<ethers.providers.TransactionResponse> => {
+	const chainConfig: any = findSupportedNetwork(env.CHAIN_HANDLE)
+
 	const provider = new ethers.providers.StaticJsonRpcProvider({
 		url: chainConfig.rpcUrl, 
 		skipFetchSetup: true // Required for ethers.js Cloudflare Worker support
@@ -551,47 +330,10 @@ const callContract = async (env: Env, chainConfig: NetworkConfig, collectionAddr
 	const walletEOA = new ethers.Wallet(env.PKEY, provider);
 	const relayerUrl = `https://${chainConfig.name}-relayer.sequence.app`
 
-	// Open a Sequence session, this will find or create
-	// a Sequence wallet controlled by your server EOA
-	const settings: Partial<SessionSettings> = {
-		networks: [{
-			...networks[chainConfig.chainId],
-			rpcUrl: chainConfig.rpcUrl,
-			provider: provider, // NOTE: must pass the provider here
-			relayer: {
-				url: relayerUrl,
-				provider: {
-					url: chainConfig.rpcUrl
-				}
-			}
-		}],
-	}
-
-	// Create a single signer sequence wallet session
-	const session = await Session.singleSigner({
-		settings: settings,
-		signer: walletEOA,
-		projectAccessKey: env.PROJECT_ACCESS_KEY_PROD
-	})
-
-	const signer = session.account.getSigner(chainConfig.chainId)
-	
-	// Standard interface for ERC1155 contract deployed via Sequence Builder
-	const collectibleInterface = new ethers.utils.Interface([
-		'function mint(address to, uint256 tokenId, uint256 amount, bytes data)'
-	])
-		
-	const data = collectibleInterface.encodeFunctionData(
-		'mint', [`${address}`, `${tokenID}`, "1", "0x00"]
-	)
-
-	const txn = {
-		to: collectionAddress, 
-		data: data
-	}
+	// TODO: implement relayer transaction
 
 	try {
-		return await signer.sendTransaction(txn)
+		return '0x' as any
 	} catch (err) {
 		console.error(`ERROR: ${err}`)
 		throw err
